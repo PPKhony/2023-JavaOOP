5c7e975c529ce5eff9b13569759505b9931d212a
ppkhon <paper2436@gmail.com>
2023-02-12 23:03:08 +0700

    remove Homework

---
+++ 2023-PracticeMidterm/arraylist/arraylist/Arraylist.java
@@ -0,0 +1,12 @@
+package arraylist;
+
+import java.util.ArrayList;
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> matrix = new ArrayList<Integer>();
+        matrix.add(5);
+        matrix.add(8);
+        System.out.println(matrix.get(1));
+        
+    }
+}
