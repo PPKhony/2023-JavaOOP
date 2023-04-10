f5235f02382e9c4bfbcc22a7f94a1d7cb48efca8
ppkhon <paper2436@gmail.com>
2023-04-10 09:38:09 +0700

    Delete 2023-Homework directory

--- 2023-Homework/Assignment4/question2/Tossable.java
+++
@@ -1,9 +0,0 @@
-package question2;
-
-public interface Tossable {
-    public String getName();
-    public int getResult();
-    public int getMinValue();
-    public int getMaxValue();
-    public void random();
-}
